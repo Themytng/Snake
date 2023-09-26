b26de6057cd93a0ea14acfd3c38775642a636086
Themytng <144147102+Themytng@users.noreply.github.com>
2023-09-26 12:08:37 -0400

    Snake Growth code and more

--- My project/Assets/New Folder/SnakeMove.cs
+++ My project/Assets/New Folder/SnakeMove.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class SnakeMove : MonoBehaviour
 {
     //variable
-    private Vector2 direction;
+    private Vector2 direction; //direction control
+
+    List<Transform> segments; //variable to store all body parts
+    public Transform bodyPrefab;   //variable to store the body
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        segments = new List<Transform>();
+        segments.Add(transform);        //add snake head to list
     }
 
     // Update is called once per frame
     void Update()
     {
+        //snake movement
         if (Input.GetKeyDown(KeyCode.W)) //when W is pressed
         {
             direction = Vector2.up;      //go up
@@ -44,5 +49,22 @@ public class SnakeMove : MonoBehaviour
             Mathf.Round(this.transform.position.x) + direction.x,  //round the number add value to X
             Mathf.Round(this.transform.position.y) + direction.y   //round the number add value to y
             );
+    }
+
+    //Function for Snake growth
+    void Grow()
+    {
+        Transform segment = Instantiate(this.bodyPrefab);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+    //Function for Collision
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
     }
 }
