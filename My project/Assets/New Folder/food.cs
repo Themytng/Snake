2884bb21b9ac7758efb3677a29736bf812366cfd
Themytng <144147102+Themytng@users.noreply.github.com>
2023-09-28 12:16:35 -0400

    save2

---
+++ My project/Assets/New Folder/food.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    //variables
+    public BoxCollider2D grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //function for randomized position
+    private void RandomPos()
+    {
+        Bounds bounds = grid.bounds;  //declare the spatial limit
+
+        float x = Random.Range(bounds.min.x, bounds.max.y); //random value of x in the limit
+        float y = Random.Range(bounds.min.y, bounds.max.y); //random value of y in the limit
+
+        transform.position = new Vector2(Mathf.Round(x), Mathf.Round(y));
+    }
+
+    //
+    void OnCollision2D(Collider2D other)
+    {
+        RandomPos();
+    }
+}
